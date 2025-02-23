00c791827b7e6d4eb9d49a9030fc261bfe0771f8
YoussefJemmane <yous.jemm@gmail.com>
2025-02-23 22:50:25 +0000

    fix:skillIcon

--- app/components/SkillIcon.tsx
+++ app/components/SkillIcon.tsx
@@ -4,11 +4,7 @@ interface SkillIconProps {
 }
 const SkillIcon: React.FC<SkillIconProps> = ({ Icon }) => (
   <div className="flex flex-col items-center mx-4">
-<<<<<<< HEAD
     <Icon className="w-[100px] h-[100px] grayscale hover:[#386FA4]" />
-=======
-    <Icon className="w-[100px] h-[100px] grayscale" />
->>>>>>> 166d5ab49765598b8a795aee6fd3b191641a98b2
   </div>
 );
 
