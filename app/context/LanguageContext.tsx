5a4c35ac240b40b1207d09a1eeedae9425108300
YoussefJemmane <yous.jemm@gmail.com>
2025-02-23 22:56:04 +0000

    fix:translation is defined but never used

--- app/context/LanguageContext.tsx
+++ app/context/LanguageContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { createContext, useContext, useState } from "react";
-import { defaultLocale, translations, Translation } from "../i18n/config";
+import { defaultLocale, translations } from "../i18n/config";
 
 type LanguageContextType = {
   locale: string;
